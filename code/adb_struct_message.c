39e8560b42778b8414fd83bba3f477559bd95f99
Simon Danner <danner.simon@gmail.com>
2014-09-11 15:10:33 +0200

    add message struct

--- code/adb_struct_message.c
+++ code/adb_struct_message.c
@@ -1,8 +1,8 @@
 struct message {
-	unsigned command; /* command identifier constant */
-	unsigned arg0; /* first argument */
-	unsigned arg1; /* second argument */
-	unsigned data_length; /* length of payload (0 is allowed) */
-	unsigned data_crc32; /* crc32 of data payload */
-	unsigned magic; /* command ^ 0xffffffff */
+	unsigned command; // command identifier 
+	unsigned arg0; // first arg
+	unsigned arg1; // second arg
+	unsigned data_length; // length payload
+	unsigned data_crc32; // crc32 payload
+	unsigned magic; // command ^ 0xffffffff
 };
